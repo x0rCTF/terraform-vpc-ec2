7e14f2f4741489f921e311015069056f0d482678
Hristijan <89606773+x0rCTF@users.noreply.github.com>
2022-03-25 00:21:50 +0100

    Delete vpc directory

--- vpc/vars.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_region" {}
-variable "vpc_cidr" {}
-variable "vpc_id" {}
-variable "subnet_cidr" {}
-variable "subnet_id" {}
-variable "vpc_tag" {}
-variable "subnet_tag" {}
-variable "sg_tag" {}
-variable "sg_name" {}
